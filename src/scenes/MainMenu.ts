f15e95a2a1d263c7ffc186955752ea560cb22816
yaroslav.safonov <yaroslav.safonov97@gmail.com>
2025-01-23 22:31:15 +0300

    add artist creds

--- src/scenes/MainMenu.ts
+++ src/scenes/MainMenu.ts
@@ -1,4 +1,4 @@
-import { Scene, GameObjects } from 'phaser';
+import { GameObjects, Scene } from 'phaser';
 
 export class MainMenu extends Scene
 {
@@ -15,10 +15,14 @@ export class MainMenu extends Scene
     {
         this.background = this.add.image(512, 384, 'background');
 
-        this.logo = this.add.image(512, 300, 'logo');
+        this.title = this.add.text(512, 300, 'Some Game for Donkey Jam', {
+            fontFamily: 'Arial Black', fontSize: 34, color: '#ffffff',
+            stroke: '#000000', strokeThickness: 8,
+            align: 'center'
+        }).setOrigin(0.5);
 
-        this.title = this.add.text(512, 460, 'Main Menu', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
+        this.title = this.add.text(512, 460, 'Art by Shyphu <3', {
+            fontFamily: 'Arial Black', fontSize: 100, color: '#ffffff',
             stroke: '#000000', strokeThickness: 8,
             align: 'center'
         }).setOrigin(0.5);
